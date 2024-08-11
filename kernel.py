c8bdbaa2954daf2ec2a4300208bb02c0a9299d12
vardhan0812-art <dialviraj@gmail.com>
2024-08-12 01:52:29 +0530

    Update kernel.py

--- kernel.py
+++ kernel.py
@@ -1,15 +1,11 @@
-import matplotlib.pyplot as plt
 import cv2
 
 # Read the image
-image = cv2.imread("image.jpg")
+img = cv2.imread('image.jpg')
 
-# Convert the image from BGR (OpenCV default) to RGB (Matplotlib expects RGB)
-image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+# Save the image to a file
+cv2.imwrite('output_image.jpg', img)
 
-# Display the image
-plt.imshow(image_rgb)
-plt.axis('off')  # Hide the axes
-plt.show()
-
-print("hwllo")
+# Optionally, open the image using an external viewer
+import os
+os.system('output_image.jpg')
